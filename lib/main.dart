75c9b779704beda8a26b28423219d02766c31ff1
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2023-11-18 11:56:56 +0530

    cooments

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
 
+// the main function, called when the app is run.
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   // This widget is the root of your application.
+
+  const MyApp({super.key}); // A constructor, we can ignore this for now.
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    // the build function returns what is shown on screen.
+
+    return const MaterialApp( // An app with the "Material" UI design scheme.
       home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  // Stateful widgets are widgets that can change over time.
+
   const MyHomePage({super.key});
 
   @override
@@ -26,9 +32,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+
+    // A simple app with a purple app bar and a centered text.
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple.shade200,
+        backgroundColor: const Color.fromARGB(255, 57, 41, 60),
         title: const Text("Birthday Card Generator"),
       ),
       body: const Center(
