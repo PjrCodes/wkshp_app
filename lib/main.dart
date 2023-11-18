7d24537e522ee01767e8fca5035b8a2edb893437
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2023-11-18 08:36:01 +0530

    date picker

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,24 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController nameController = TextEditingController();
+
+  DateTime selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate,
+      firstDate: DateTime(2000), // Actually 01/01/2000
+      lastDate: DateTime(2024),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,9 +49,36 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.purple.shade200,
         title: const Text("Birthday Card Generator"),
       ),
-      body: const Center(
-        child: Text("Hello World!"),
-      ), // The comma at the end makes everything look nicer.
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: 400.0,
+            child: TextField(
+              controller: nameController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your name',
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text("${selectedDate.toLocal()}".split(' ')[0]),
+          const SizedBox(
+            height: 20.0,
+          ),
+          ElevatedButton(
+            onPressed: () => _selectDate(context),
+            child: const Text('Select date'),
+          ),
+          const SizedBox(height: 20.0,),
+          ElevatedButton(
+            child: const Text("Create Card"),
+            onPressed: () {},
+          )
+        ],
+      )), // The comma at the end makes everything look nicer.
     );
   }
 }
